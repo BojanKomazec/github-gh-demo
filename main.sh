c54395affeffb61b85f6b2ba72e0e2bee62186d9
Bojan Komazec <1876278+BojanKomazec@users.noreply.github.com>
2025-03-24 14:58:59 +0000

    feat: function which demoes finding workflows in the current or given repository

--- main.sh
+++ main.sh
@@ -14,17 +14,71 @@ get_current_branch() {
     # Check if branch is found
     if [[ -z "$BRANCH" ]]; then
         echo "❌ Failed to determine current branch."
-        exit 1
+        return 1
     fi
 
     echo "🚀 Current script is running on branch: $BRANCH"
 }
 
+fetch_worfklows() {
+    # Fetch list of workflows
+    echo "🔍 Fetching available workflows..."
+    WORKFLOWS=$(gh workflow list --json name --jq '.[].name')
+    WORKFLOWS=($WORKFLOWS)
+
+    # Check if workflows are found
+    if [[ -z "$WORKFLOWS" ]]; then
+        echo "❌ No workflows found."
+        return 1
+    fi
+
+    echo "✅ Workflows found:"
+    # Iterate over workflows
+    for workflow in "${WORKFLOWS[@]}"; do
+        echo "  - $workflow"
+    done
+}
+
+fetch_worfklows_in_repo() {
+    REPO=$1
+
+    # Fetch list of workflows
+    echo "🔍 Fetching available workflows in repository: $REPO..."
+    response=$(gh workflow list --repo $REPO --json name)
+    if [[ $? -ne 0 ]]; then
+        echo "❌ Failed to fetch workflows in repository: $REPO."
+        return 1
+    fi
+
+    echo $response | jq .
+
+    WORKFLOWS=$(gh workflow list --repo $REPO --json name --jq '.[].name')
+
+    # Convert this to array
+    WORKFLOWS=($WORKFLOWS)
+
+    # Check if workflows are found
+    if [[ -z "$WORKFLOWS" ]]; then
+        echo "❌ No workflows found in repository: $REPO."
+        return 1
+    fi
+
+    echo "✅ Workflows found in repository: $REPO"
+    # Iterate over workflows
+    for workflow in "${WORKFLOWS[@]}"; do
+        echo "  - $workflow"
+    done
+}
+
 main() {
     check_if_installed "git"
     check_if_installed "gh"
+    check_if_installed "jq"
 
     get_current_branch
+    fetch_worfklows
+    fetch_worfklows_in_repo "bojankomazec/github-gh-demo"
+    fetch_worfklows_in_repo "hashicorp/terraform"
 }
 
 main "$@"
